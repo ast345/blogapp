c56666627555667de157b6dd5bb7e6acc5d0159b
小川敦司 <>
2023-02-05 15:02:35 +0900

    フォロー機能の実装（コマンド上でのみ動作）

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,12 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :birthday, :gender, :age, to: :profile, allow_nil: true
 
   def has_wtitten?(article)
@@ -50,6 +56,15 @@ class User < ApplicationRecord
 
   def has_liked?(article)
     likes.exists?(article_id: article.id)
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relationship = following_relationships.find_by!(following_id: user.id)
+    relationship.destroy!
   end
 
 
