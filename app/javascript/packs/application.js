d797189826df493929b4671c264772d2a382ddb8
小川敦司 <>
2023-02-11 12:20:16 +0900

    いいねのdb更新をjsに移行

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,17 @@ require("@rails/actiontext")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+const handleHeartDisplay = (hasLiked) => {
+    if (hasLiked) {
+      $('.active-heart').removeClass('hidden')
+    } else {
+      $('.inactive-heart').removeClass('hidden')
+    }
+  }
 
 document.addEventListener('turbolinks:load', () => {
     const dataset = $('#article-show').data()
@@ -28,10 +39,28 @@ document.addEventListener('turbolinks:load', () => {
     axios.get(`/articles/${articleId}/like`)
 	    .then((response) => {
             const hasLiked = response.data.hasLiked
-            if (hasLiked){
-	            $('.active-heart').removeClass('hidden')
-            } else {
-	            $('.inactive-heart').removeClass('hidden')
-            }
+            handleHeartDisplay(hasLiked)
 	    })
+    
+    $('.inactive-heart').on('click', () => {
+        axios.post(`/articles/${articleId}/like`)
+            .then((response) => {
+            console.log(response)
+            })
+            .catch((e) => {
+            window.alert('Error')
+            console.log(e)
+            })
+        })
+    
+    $('.active-heart').on('click', () => {
+        axios.delete(`/articles/${articleId}/like`)
+            .then((response) => {
+            console.log(response)
+            })
+            .catch((e) => {
+            window.alert('Error')
+            console.log(e)
+            })
+    })
 })
