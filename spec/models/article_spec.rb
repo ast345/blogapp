7fda05421a07a6e0f7c409ccfd42cb5dd81f803e
小川敦司 <>
2023-02-23 12:54:14 +0900

    titleの文字数に関するエラーのtest

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,24 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  context 'タイトルと内容が表示されている場合' do
-    let!(:user) do
-      User.create!({
-        email: 'd@d.com',
-        password: 'password'
-      })
-    end
-
-    let!(:article) do
-      user.articles.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 300)
-      })
-    end
-
-    it 'タイトルと内容が入力されていれば、記事を保存できる' do
-      expect(article).to be_valid
-    end
+  let!(:user) do
+    User.create!({
+      email: 'd@d.com',
+      password: 'password'
+    })
   end
 
+	context 'タイトルと内容が入力されている場合' do
+		let!(:article) do
+			user.articles.build({
+				title: Faker::Lorem.characters(number: 10),
+	      content: Faker::Lorem.characters(number: 300)
+			})
+		end
+
+		it 'タイトルと内容が入力されていれば、記事を保存できる' do
+			expect(article).to be_valid
+		end
+	end
+
+	context 'タイトルの文字が一文字の場合' do
+		let!(:article) do
+			user.articles.create({
+				title: Faker::Lorem.characters(number: 1),
+	      content: Faker::Lorem.characters(number: 300)
+			})
+		end
+
+		it '記事を保存できない' do
+			expect(article.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
+		end
+	end
 end
